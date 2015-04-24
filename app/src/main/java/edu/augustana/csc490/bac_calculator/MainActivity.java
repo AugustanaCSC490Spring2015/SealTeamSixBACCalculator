544dcb4bd0385a2b7187bf27cf80f3ceb60dd8b9
lindseynicolaysen11 <lindseynicolaysen11@augustana.edu>
2015-04-24 15:54:26 -0500

    Added dashboard xml, created graph

--- app/src/main/java/edu/augustana/csc490/bac_calculator/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/bac_calculator/MainActivity.java
@@ -9,6 +9,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import edu.augustana.csc490.bac_calculator.utils.Constants;
 
 
@@ -36,6 +40,22 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
+
+        //graph view
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+
+
+
     }
 
     @Override
