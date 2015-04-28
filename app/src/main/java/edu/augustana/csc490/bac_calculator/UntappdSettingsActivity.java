f838626d65c0c9f370d63aca8418ad1c55aed754
Dan Shultz <dan@danshultz.com>
2015-04-27 21:33:10 -0500

    Added log out of Untappd feature

--- app/src/main/java/edu/augustana/csc490/bac_calculator/UntappdSettingsActivity.java
+++ app/src/main/java/edu/augustana/csc490/bac_calculator/UntappdSettingsActivity.java
@@ -8,7 +8,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
@@ -60,64 +59,83 @@ public class UntappdSettingsActivity extends ActionBarActivity {
             WebView webView;
             @Override
             public void onClick(View view) {
-                dialog = new Dialog(UntappdSettingsActivity.this);
-                dialog.setContentView(R.layout.auth_dialog);
-                dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
-                        WindowManager.LayoutParams.MATCH_PARENT);
-                webView = (WebView) dialog.findViewById(R.id.webView);
-                webView.getSettings().setJavaScriptEnabled(true); // enable javascript in the WebView
+                // Check if the user is logging in out out
+                if (sharedPreferences.getString(Constants.PREF_UNTAPPD_TOKEN, null) != null) {
+                    // Log out - Delete token,  first name, last name, and username in prefs
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(Constants.PREF_UNTAPPD_TOKEN, null);
+                    editor.putString(Constants.PREF_USER_FIRST_NAME, null);
+                    editor.putString(Constants.PREF_USER_LAST_NAME, null);
+                    editor.putString(Constants.PREF_USER_USERNAME, null);
+                    editor.commit();
 
-                // Load the page for the user to authenticate the app. Link in form of
-                // https://untappd.com/oauth/authenticate/?client_id=CLIENTID&response_type=token&redirect_url=REDIRECT_URL
-                webView.loadUrl(Constants.UNTAPPD_AUTH_URL);
+                    authAppButton.setText(R.string.auth_app_btn);
+                    untappdUserInfo.setText("");
+                } else {
+                    // User is logging in
+                    dialog = new Dialog(UntappdSettingsActivity.this);
+                    dialog.setContentView(R.layout.auth_dialog);
+                    dialog.getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT,
+                            WindowManager.LayoutParams.MATCH_PARENT);
+                    webView = (WebView) dialog.findViewById(R.id.webView);
+                    webView.getSettings().setJavaScriptEnabled(true); // enable javascript in the WebView
 
-                // Set the custom WebViewClient
-                webView.setWebViewClient(new WebViewClient() {
-                    boolean haveToken = false;
-                    String token;
+                    // Load the page for the user to authenticate the app. Link in form of
+                    // https://untappd.com/oauth/authenticate/?client_id=CLIENTID&response_type=token&redirect_url=REDIRECT_URL
+                    webView.loadUrl(Constants.UNTAPPD_AUTH_URL);
 
-                    @Override
-                    public void onPageFinished(WebView view, String url) {
-                        super.onPageFinished(view, url);
+                    // Set the custom WebViewClient
+                    webView.setWebViewClient(new WebViewClient() {
+                        boolean haveToken = false;
+                        String token;
 
-                        // Check if the url contains the token
-                        if (url.contains("token=") && !haveToken) {
-                            String removeFromUrl = Constants.REDIRECT_URL + "/#access_token=";
-                            token = url.substring(removeFromUrl.length());
-                            haveToken = true;
+                        @Override
+                        public void onPageFinished(WebView view, String url) {
+                            super.onPageFinished(view, url);
 
-                            // Save token in preferences
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString(Constants.PREF_UNTAPPD_TOKEN, token);
-                            editor.commit();
+                            // Check if the url contains the token
+                            if (url.contains("token=") && !haveToken) {
+                                String removeFromUrl = Constants.REDIRECT_URL + "/#access_token=";
+                                token = url.substring(removeFromUrl.length());
+                                haveToken = true;
 
-                            // update user info on screen
-                            updateUserInfo();
+                                // Save token in preferences
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString(Constants.PREF_UNTAPPD_TOKEN, token);
+                                editor.commit();
 
-                            // close dialog
-                            dialog.dismiss();
+                                // update user info on screen
+                                new GetUserInfo().execute();
+
+                                // close dialog
+                                dialog.dismiss();
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                        super.onPageStarted(view, url, favicon);
-                    }
-                });
+                        @Override
+                        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                            super.onPageStarted(view, url, favicon);
+                        }
+                    });
 
-                // Set the title and show the dialog
-                dialog.setTitle(getResources().getString(R.string.auth_untappd_dialog_title));
-                dialog.show();
+                    // Set the title and show the dialog
+                    dialog.setTitle(getResources().getString(R.string.auth_untappd_dialog_title));
+                    dialog.show();
+                }
             }
         });
 
     } // end method onCreate
 
     private void updateUserInfo() {
-        // User is already authorized, disable button
-        authAppButton.setEnabled(false);
+        authAppButton.setText(R.string.sign_out);
 
-        new GetUserInfo().execute();
+        String first_name = sharedPreferences.getString(Constants.PREF_USER_FIRST_NAME, "");
+        String last_name = sharedPreferences.getString(Constants.PREF_USER_LAST_NAME, "");
+        String user_name = sharedPreferences.getString(Constants.PREF_USER_USERNAME, "");
+
+        String userInfo = "Logged in as " + first_name + " " + last_name + " (" + user_name +")";
+        untappdUserInfo.setText(userInfo);
     }
 
     // Note: AsyncTask must be subclassed to be used.
@@ -150,30 +168,20 @@ public class UntappdSettingsActivity extends ActionBarActivity {
             if (jsonObject != null) {
                 try {
                     JSONObject userJSONObject = jsonObject.getJSONObject(Constants.TAG_RESPONSE).getJSONObject(Constants.TAG_USER);
-                    JSONObject statsJSONObject = userJSONObject.getJSONObject(Constants.TAG_STATS);
 
                     // Get user info
                     String first_name = userJSONObject.getString(Constants.TAG_FIRST_NAME);
                     String last_name = userJSONObject.getString(Constants.TAG_LAST_NAME);
                     String user_name = userJSONObject.getString(Constants.TAG_USER_NAME);
-                    // Get user stats
-                    String total_badges = statsJSONObject.getString(Constants.TAG_TOTAL_BADGES);
-                    String total_friends = statsJSONObject.getString(Constants.TAG_TOTAL_FRIENDS);
-                    String total_checkins = statsJSONObject.getString(Constants.TAG_TOTAL_CHECKINS);
-                    String total_beers = statsJSONObject.getString(Constants.TAG_TOTAL_BEERS);
 
-                    String userInfo = "User Info:\n" +
-                            "First Name: " + first_name + "\n" +
-                            "Last Name: " + last_name + "\n" +
-                            "Username: " + user_name + "\n\n" +
-                            "Stats:"+ "\n" +
-                            "Distinct Beers: " + total_beers + "\n" +
-                            "Check-Ins: " + total_checkins + "\n" +
-                            "Badges: " + total_badges + "\n" +
-                            "Friends: " + total_friends;
+                    // Save first name, last name, and username in prefs
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(Constants.PREF_USER_FIRST_NAME, first_name);
+                    editor.putString(Constants.PREF_USER_LAST_NAME, last_name);
+                    editor.putString(Constants.PREF_USER_USERNAME, user_name);
+                    editor.commit();
 
-                    untappdUserInfo.setText(userInfo);
-
+                    updateUserInfo();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
