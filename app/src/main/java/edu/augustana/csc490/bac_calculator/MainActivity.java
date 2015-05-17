b2eee5ea6d16c9e3047008fd12a261bed9dfb53b
Dan Shultz <dan@danshultz.com>
2015-05-17 17:57:35 -0500

    Fixed last push

--- app/src/main/java/edu/augustana/csc490/bac_calculator/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/bac_calculator/MainActivity.java
@@ -274,6 +274,12 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        drinkAdapter.notifyDataSetChanged();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
